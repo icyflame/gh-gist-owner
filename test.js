9eefa037460126957d66352943594050f2d5448a
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-20 14:23:44 +0530

    Add a test
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ var assert = require('assert');
 var ghGistOwner = require('./');
 
 it('should ', function () {
-	assert.strictEqual(ghGistOwner('unicorns'), 'unicorns & rainbows');
+	assert.strictEqual(ghGistOwner('420166ca54b7afe55476'), 'icyflame');
 });

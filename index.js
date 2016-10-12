aaac724c499be0e77ddd96cb6e46259210b821e2
Tom Harding <tomjharding@live.co.uk>
2016-10-12 07:20:33 +0100

    Implement PR change requests
    @icyflame's requests have been fulfilled as per PR#2.

--- index.js
+++ index.js
@@ -13,13 +13,25 @@ var url = function (id) {
   return 'https://api.github.com/gists/' + id;
 };
 
-exports.sync = function (str) {
+module.exports = function (str) {
   var res = syncRequest('GET', url(str), options);
   return JSON.parse(res.getBody()).owner.login;
 };
 
-exports.async = function (str, cb) {
-  asyncRequest('GET', url(str), options).done(function (res) {
-    cb(JSON.parse(res.getBody()).owner.login);
-  });
+module.exports.sync = module.exports;
+
+module.exports.async = function (str, cb) {
+  asyncRequest(
+    'GET',
+    url(str),
+    options,
+    function (err, res) {
+      var data = err
+        ? null
+        : JSON.parse(
+            res.getBody()
+        ).owner.login;
+
+      cb(err, data);
+    });
 };
--- test.js
+++ test.js
@@ -4,6 +4,10 @@
 var assert = require('assert');
 var ghGistOwner = require('./');
 
+it('defaults correctly', function () {
+  assert.strictEqual(ghGistOwner, ghGistOwner.sync);
+});
+
 it('gets the owner', function () {
   assert.strictEqual(
     ghGistOwner.sync('420166ca54b7afe55476'),
@@ -14,8 +18,9 @@ it('gets the owner', function () {
 it('gets the owner asynchronously', function (done) {
   ghGistOwner.async(
     '420166ca54b7afe55476',
-    function (owner) {
+    function (err, owner) {
       assert.strictEqual(owner, 'icyflame');
+      assert.strictEqual(err, null);
       done();
     }
   );
